0effd5dda5cbcac65d0d85f6207d1e8a97c4bdc8
AntonioCastro1163 <antonio.castro@ieeg.org.mx>
2024-05-14 00:32:37 -0600

    Corregido el problema de las imagenes

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
     //cambiar la imagen de la pestaña
     const favicon = document.querySelector('link[rel="icon"]')
     if(favicon){
-      favicon.href = "./src/components/sol.png"
+      favicon.href = "./src/components/sol.pmg"
     }
     
     setTimeout(() => {
